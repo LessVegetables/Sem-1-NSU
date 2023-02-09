107bc98d6519bfac8b076a7f6f59c2b45f9e00a7
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2023-02-10 00:04:40 +0700

    more bug fixes

--- Lesson 1/1.c
+++ Lesson 1/1.c
@@ -19,40 +19,54 @@ int main()
     printf("S0:\t%f\nS1:\t%f\nS2:\t%f\nS3\t%f\n", S0, S1, S2, S3);
     if (S0 == S1 + S2 + S3)
     {
-        printf("inside");
+        //printf("inside\n");
+        state = 1; /*inside*/
     }
-
-
-
-    float k1, b1, k2, b2, k3, b3;
-
-    k1 = (y1 - y2) / (x1 - x2);
-    k2 = (y2 - y3) / (x2 - x3);
-    k3 = (y3 - y1) / (x3 - x1);
-    b1 = y1 - k1 * x1;
-    b2 = y2 - k2 * x2;
-    b3 = y3 - k3 * x3;
-
-    int match1 = 0, match2 = 0, match3 = 0;
-
-    if ((k2*x1 + b2 - y1)*b2 >= 0){ match1 = 1;}
-    if ((k3*x2 + b3 - y3)*b3 >= 0){ match2 = 1;}
-    if ((k1*x3 + b1 - y3)*b3 >= 0){ match3 = 1;}
-
-    if (match1 * match2 * match3 == 1) { state = 1; /*inside*/}
-    else if (match1 * match2 * match3 == 0) { state = 0; /*outside*/}
+    else { state = 0; /*outside*/}
 
     if (state)
     {
+        printf("checking if on the side\n");
+        float k1, b1, k2, b2, k3, b3;
+
+        k1 = (y1 - y2) / (x1 - x2);
+        k2 = (y2 - y3) / (x2 - x3);
+        k3 = (y3 - y1) / (x3 - x1);
+        b1 = y1 - k1 * x1;
+        b2 = y2 - k2 * x2;
+        b3 = y3 - k3 * x3;
+        printf("b1:\t%f\nb2:\t%f\nb3:\t%f\n", b1, b2, b3);
         if (b1*b2*b3 == 0)
         {
             state = 2; /*on the side*/
         }
     }
 
-    if (state == 0){ printf("outside");}
-    if (state == 1){ printf("inside");}
-    if (state == 2){ printf("on the side");}
+
+
+
+    
+
+    // int match1 = 0, match2 = 0, match3 = 0;
+
+    // if ((k2*x1 + b2 - y1)*b2 >= 0){ match1 = 1;}
+    // if ((k3*x2 + b3 - y3)*b3 >= 0){ match2 = 1;}
+    // if ((k1*x3 + b1 - y3)*b3 >= 0){ match3 = 1;}
+
+    // if (match1 * match2 * match3 == 1) { state = 1; /*inside*/}
+    // else if (match1 * match2 * match3 == 0) { state = 0; /*outside*/}
+
+    // if (state)
+    // {
+    //     if (b1*b2*b3 == 0)
+    //     {
+    //         state = 2; /*on the side*/
+    //     }
+    // }
+
+    if (state == 0){ printf("outside\n");}
+    if (state == 1){ printf("inside\n");}
+    if (state == 2){ printf("on the side\n");}
 
     printf("%f %f, %f %f, %f %f", x1, y1, x2, y2, x3, y3);
     
