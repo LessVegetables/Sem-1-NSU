4ab91d24a1eb5b906a4afd0b65a75835e641f1aa
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2023-02-09 23:13:13 +0700

    bug fixes

--- Lesson 1/1.c
+++ Lesson 1/1.c
@@ -6,18 +6,18 @@ int main()
     float x1, y1, x2, y2, x3, y3;
     int state = 0;
     printf("Enter the coordinates: ");
-    scanf("%f %f %f %f %f %f", x1, y1, x2, y2, x3, y3);
+    scanf("%f %f %f %f %f %f", &x1, &y1, &x2, &y2, &x3, &y3);
 
 
 
     float S0, S1, S2, S3;
-    S0 = 0.5 * abs((x2 - x1)*(y3 - y1) - (x3 - x1)*(y2 - y1));
-    S1 = 0.5 * abs((x2 - 0)(y3 - 0) - (x3 - 0)*(y2 - y1));
-    S2 = 0.5 * abs((0 - x1)*(y3 - y1) - (x3 - x1)*(0 - y1));
-    S3 = 0.5 * abs((x2 - x1)*(0 - y1) - (0 - x1)*(y2 - y1));
+    S0 = 0.5 * fabs((x2 - x1)*(y3 - y1) - (x3 - x1)*(y2 - y1));
+    S1 = 0.5 * fabs((x2 - 0)*(y3 - 0) - (x3 - 0)*(y2 - y1));
+    S2 = 0.5 * fabs((0 - x1)*(y3 - y1) - (x3 - x1)*(0 - y1));
+    S3 = 0.5 * fabs((x2 - x1)*(0 - y1) - (0 - x1)*(y2 - y1));
 
     printf("S0:\t%f\nS1:\t%f\nS2:\t%f\nS3\t%f\n", S0, S1, S2, S3);
-    if (S0 = S1 + S2 + S3)
+    if (S0 == S1 + S2 + S3)
     {
         printf("inside");
     }
